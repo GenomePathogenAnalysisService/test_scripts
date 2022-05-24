5fa56799fa984f4bd8518c2ebdb4d4a3237ba45a
Marcela Alves <marcela.config@gmail.com>
2022-05-24 13:29:57 +0100

    Update generate_gpas_uploader_csv.sh

--- generate_gpas_uploader_csv.sh
+++ generate_gpas_uploader_csv.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
 
-cd illumina/
+cd ../gpas-covid-synthetic-reads/illumina/
 
-python3 ../../gpas-covid-synthetic-reads/bin/gpas-build-uploadcsv.py --country SWE --tech illumina --tag_file ../tags.txt --uuid_length short --number_of_tags 1 > illumina.csv
+gpas-build-uploadcsv.py --country SWE --tech illumina --tag_file ../../test_scripts/tags.txt --uuid_length short --number_of_tags 1 > illumina.csv
 
-gzip *fastq
 
 cd ../nanopore/
 
-python3  ../../gpas-covid-synthetic-reads/bin/gpas-build-uploadcsv.py --country NOR --tech nanopore --tag_file ../tags.txt --uuid_length short --number_of_tags 1 > nanopore.csv
+gpas-build-uploadcsv.py --country NOR --tech nanopore --tag_file ../../test_scripts/tags.txt --uuid_length short --number_of_tags 1 > nanopore.csv
 
  
