e8727748f30928cf5d972c8ac37bfec9f089bb1d
Marcela Alves <marcela.config@gmail.com>
2022-06-06 23:50:15 +0100

    Update generate_synthetic_data.sh

--- generate_synthetic_data.sh
+++ generate_synthetic_data.sh
@@ -3,7 +3,7 @@ for tech in illumina nanopore; do
     for primer in articv3 articv4 midnight1200; do
         for snps in 4; do
             for coverage in 500; do
-                for error in 1 8; do
+                for error in 1; do
                     for lineage in cB.1.1.7 cB.1.617.2 cB.1.1.529 cBA.1 cBA.2 cBA.3; do
                         echo "$primer : $snps - $coverage -- error: $error -- lineage $lineage"
                         gpas-covid-synreads-create.py\
