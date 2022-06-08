490c2f17864c30e8f2d680920b4aa58b09154f22
Marcela Alves <marcela.config@gmail.com>
2022-06-08 12:51:08 +0100

    Update generate_gpas_uploader_csv.sh

--- generate_gpas_uploader_csv.sh
+++ generate_gpas_uploader_csv.sh
@@ -6,9 +6,13 @@ cd illumina
 
 gpas-build-uploadcsv.py --country SWE --tech illumina --tag_file ../../test_scripts/tags.txt --uuid_length short --number_of_tags 1 > illumina.csv
 
+gzip *fastq
+
 cd ../nanopore/
 
 gpas-build-uploadcsv.py --country NOR --tech nanopore --tag_file ../../test_scripts/tags.txt --uuid_length short --number_of_tags 1 > nanopore.csv
+
+gzip *fastq
 
 cd ..
 
