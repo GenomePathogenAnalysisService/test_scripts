3119da8d9145d8cde3f03e65daccf4d4db7a7333
Marcela Alves <marcela.config@gmail.com>
2022-06-09 16:50:36 +0100

    Update test_nanopore.sh

--- test_nanopore.sh
+++ test_nanopore.sh
@@ -4,6 +4,8 @@
 #Nanopore
 gpas-upload --token ../test_scripts/token.json --json --environment dev download ../gpas-covid-synthetic-reads/nanopore/sample_names.csv --file_types fasta json --rename
 
+gunzip *fasta.gz
+
 gpas-covid-synreads-analyse.py --output nanopore-results.csv.gz --input ../gpas-covid-synthetic-reads/nanopore/sample_names.csv
 
 mkdir "tests_nanopore"
